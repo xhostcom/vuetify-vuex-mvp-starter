464c3f506983c4e880f74f9696552bacd9f0d83a
Paul Anthony McGowan <xhostcom@gmail.com>
2020-04-02 23:19:44 +0100

    First Commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import '@/plugins/axios'
+import '@/plugins/vuetify'
+import '@/plugins/googleAnalytics'
+import i18n from '@/plugins/i18n'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.config.productionTip = false
+
+const app = new Vue({
+  vuetify,
+  router,
+  store,
+  i18n,
+  render: h => h(App),
+  created() {
+    store.dispatch('setLocale', store.getters.locale)
+    if (store.getters.isTokenSet) {
+      store.dispatch('autoLogin')
+    }
+  }
+}).$mount('#app')
+
+if (window.Cypress) {
+  // Only available during E2E tests
+  window.app = app
+}
+
+
